cc100b2ab3745de75ef1eb0a3694a75a9cd34374
Smit <smitdesai2130@gmail.com>
2021-05-26 14:52:52 +0530

    Hidden some files ✅

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import {getSession} from 'next-auth/client';
+import { getSession } from 'next-auth/client';
 import Head from 'next/head';
 import Header from '../components/Header';
-import Login from '../components/Login.js'
+import Login from '../components/Login.js';
 
-export default function Home({session}) {
+export default function Home({ session }) {
   //passed the session prop
 
-  if(!session) return <Login />;
+  if (!session) return <Login />;
 
   return (
     <div>
@@ -25,22 +25,21 @@ export default function Home({session}) {
   );
 }
 
-
 // Incooperate sever side rendering in our next js app
 // This will tell next js that now we are gonna have ssr
 
-export async function getServerSideProps(context){
+export async function getServerSideProps(context) {
   //Get the user props
   const session = await getSession(context);
 
-//! Our user makes a request to go to oursite.com wherever your nextjs application is
-// That request hits the server, the server then gets the user's session our the logged in/out 
-// user is waiting at that point right and sever has that information about it so server basically prepares the page 
-// and that session information it passes it to the component as props and then all this information is the rendered on the user's browser
-  return{
-    props:{
-      session
-      // NOW passing this prop it above as in Home Component 
-    }
-  }
+  //! Our user makes a request to go to oursite.com wherever your nextjs application is
+  // That request hits the server, the server then gets the user's session our the logged in/out
+  // user is waiting at that point right and sever has that information about it so server basically prepares the page
+  // and that session information it passes it to the component as props and then all this information is the rendered on the user's browser
+  return {
+    props: {
+      session,
+      // NOW passing this prop it above as in Home Component
+    },
+  };
 }
