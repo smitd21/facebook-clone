3cf2b77b62317ca0b1f71cefa06257f24a8b8c3f
Smit <smitdesai2130@gmail.com>
2021-05-26 23:40:09 +0530

    react-firebase-hooks to render from realtimedb & start Post component🔥

---
+++ components/Post.js
@@ -0,0 +1,9 @@
+function Post({ name, message, email, image, postImage, timestamp }) {
+  return (
+    <div>
+      <h1>A post</h1>
+    </div>
+  );
+}
+
+export default Post;
--- components/Posts.js
+++ components/Posts.js
@@ -1,5 +1,30 @@
+import { useCollection } from 'react-firebase-hooks/firestore'; //! Awesome hook
+import { db } from '../firebase';
+import Post from './Post';
+
 function Posts() {
-  return <div></div>;
+  const [realtimePosts] = useCollection(
+    //look inside db and display them order by timestamp - most recent at the top
+    db.collection('posts').orderBy('timestamp', 'desc')
+  );
+  return (
+    <div>
+      {/* list of docs me se render each post */}
+      {realtimePosts?.docs.map((post) => (
+        //   for every single post render Post component
+        <Post
+          key={post.id}
+          //   to acces the data of post inside we do .data(). - we pushed all of these and now render it
+          name={post.data().name}
+          message={post.data().message}
+          email={post.data().email}
+          timestamp={post.data().timestamp}
+          image={post.data().image}
+          postImage={post.data().postImage}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Posts;
