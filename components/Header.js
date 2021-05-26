8a55099ab227faa0cbcb7ce0f4be69eab9695b0a
Smit <smitdesai2130@gmail.com>
2021-05-26 14:49:49 +0530

    Pull session auth info to header ✅

--- components/Header.js
+++ components/Header.js
@@ -14,8 +14,10 @@ import {
   ShoppingCartIcon,
 } from '@heroicons/react/outline';
 import HeaderIcon from './HeaderIcon';
+import { signOut, useSession } from 'next-auth/client';
 
 function Header() {
+  const [session] = useSession(); //! useSession hook to pull th session information from anywhere within the app
   return (
     <div
       className="sticky top-0 z-50 bg-white flex
@@ -54,6 +56,14 @@ function Header() {
       {/* Right */}
       <div className="flex items-center sm:space-x-2 justify-end">
         {/* Profile pic*/}
+        <Image
+          onClick={signOut}
+          className="rounded-full cursor-pointer"
+          src={session.user.image}
+          width="40"
+          height="40"
+          layout="fixed"
+        />
 
         <p className="whitespace-nowrap font-semibold pr-3">Smit Desai</p>
         <ViewGridIcon className="icon" />
